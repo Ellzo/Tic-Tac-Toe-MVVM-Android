5c41bfd1d0f286611841800d3ca780b650cff02f
Ellzo <ellzo1808@gmail.com>
2023-10-18 21:42:36 +0100

    Heuristic search done

--- app/src/main/java/dev/startsoftware/tictactoe/viewmodels/GameViewModel.kt
+++ app/src/main/java/dev/startsoftware/tictactoe/viewmodels/GameViewModel.kt
@@ -1,5 +1,7 @@
 package dev.startsoftware.tictactoe.viewmodels
 
+import android.util.Log
+import android.util.Pair
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,6 +9,8 @@ import dev.startsoftware.tictactoe.models.GameState
 import dev.startsoftware.tictactoe.models.Board
 import dev.startsoftware.tictactoe.models.Cell
 import dev.startsoftware.tictactoe.models.Player
+import kotlin.math.pow
+import kotlin.math.sqrt
 import kotlin.properties.Delegates
 import kotlin.random.Random
 
@@ -42,14 +46,17 @@ class GameViewModel(private val size: Int): ViewModel() {
     }
 
     fun makeMove(position: Int): Boolean{
-        if(!initiateOrOnGoing())
-            return false
-
         val x = position % size
         val y = position / size
+        return makeMove(x, y)
+    }
 
-        if(_liveBoardState.value!!.cells[x][y] == Cell.EMPTY) {
-            _liveBoardState.value!!.cells[x][y] = if(liveTurn.value!!.isX) Cell.X() else Cell.O()
+    private fun makeMove(x: Int, y: Int): Boolean {
+        if (!initiateOrOnGoing() || x !in 0 until size || y !in 0 until size)
+            return false
+
+        if (_liveBoardState.value!!.cells[x][y] == Cell.EMPTY) {
+            _liveBoardState.value!!.cells[x][y] = if (liveTurn.value!!.isX) Cell.X() else Cell.O()
             updateGameStatus(x, y)
             _liveBoardState.notifyObserver()
 
@@ -68,12 +75,144 @@ class GameViewModel(private val size: Int): ViewModel() {
         if(!(liveTurn.value is Player.Player2 && (liveTurn.value as Player.Player2).isComputer))
             return
 
-        var isDone: Boolean
+        //randomComputerMove()
+        heuristicComputerMove()
+    }
+
+    private fun randomComputerMove(){
         var position: Int
-        do{
+        while(true){
             position = Random.nextInt(size * size)
-            isDone = makeMove(position)
-        }while(!isDone)
+            if(makeMove(position))
+                return
+        }
+    }
+
+    private fun heuristicComputerMove(): Boolean{
+        val board = liveBoardState.value!!
+
+        val horizontalScores = horizontalHeuristics()
+        val verticalScores = verticalHeuristics()
+        val diagonalScore = diagonalHeuristic()
+        val antiDiagonalScore = antiDiagonalHeuristic()
+
+        var score: Int
+        var minScore: Int = size * size + 1
+        var minX: Int = -1
+        var minY: Int = -1
+
+        for(x in 0 until board.size){
+            for(y in 0 until board.size){
+                if(board.cells[x][y] is Cell.EMPTY){
+                    score = minOf(horizontalScores[x], verticalScores[y])
+                    if(inMainDiagonal(x, y))
+                        score = minOf(score, diagonalScore)
+                    if(inAntiDiagonal(x, y))
+                        score = minOf(score, antiDiagonalScore)
+
+                    if(score < minScore) {
+                        minScore = score
+                        minX = x
+                        minY = y
+                    }
+
+                    if(score == minScore){
+                        val distMin = sqrt((minX - size/2.0).pow(2) + (minY - size/2.0).pow(2))
+                        val dist = sqrt((x - size/2.0).pow(2) + (y - size/2.0).pow(2))
+                        if(dist < distMin && Random.nextInt(10) < 6){
+                            minX = x
+                            minY = y
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return makeMove(minX, minY)
+    }
+
+    private fun inMainDiagonal(x: Int, y: Int) = x == y
+
+    private fun inAntiDiagonal(x: Int, y: Int) = x + y == size - 1
+
+    private fun horizontalHeuristics(): Array<Int>{
+        val heuristics = Array(size){ 2 * size }
+        for(row in 0 until size){
+            heuristics[row] = calcHeuristicScore(liveBoardState.value!!.cells[row])
+        }
+
+        return heuristics
+    }
+
+    private fun verticalHeuristics(): Array<Int>{
+        val heuristics = Array(size){ 2 * size }
+
+        for(y in 0 until size){
+            val columns = Array<Cell>(size){ Cell.EMPTY }
+            for(x in 0 until size){
+                columns[x] = liveBoardState.value!!.cells[x][y]
+            }
+            heuristics[y] = calcHeuristicScore(columns)
+        }
+
+        return heuristics
+    }
+
+    private fun diagonalHeuristic(): Int{
+        val diagonals = Array<Cell>(size){ Cell.EMPTY }
+        for(xy in 0 until size)
+            diagonals[xy] = liveBoardState.value!!.cells[xy][xy]
+
+        return calcHeuristicScore(diagonals)
+    }
+
+    private fun antiDiagonalHeuristic(): Int{
+        val antiDiagonals = Array<Cell>(size){ Cell.EMPTY }
+        for(xy in 0 until size)
+            antiDiagonals[xy] = liveBoardState.value!!.cells[xy][size - xy - 1]
+
+        return calcHeuristicScore(antiDiagonals)
+    }
+
+    private fun calcHeuristicScore(cells: Array<Cell>): Int{
+        var score = size * (size - 1)
+        var encounteredX = false
+        var encounteredO = false
+
+        for(cell in cells){
+            if(cell is Cell.EMPTY)
+                continue
+
+            score -= when{
+                (cell is Cell.X && liveTurn.value!!.isX) -> {
+                    encounteredX = true
+                    size
+                }
+
+                (cell is Cell.O && !liveTurn.value!!.isX) -> {
+                    encounteredO = true
+                    size
+                }
+
+                (cell is Cell.X && !liveTurn.value!!.isX) -> {
+                    encounteredX = true
+                    size - 1
+                }
+
+                (cell is Cell.O && liveTurn.value!!.isX) -> {
+                    encounteredO = true
+                    size - 1
+                }
+
+                else -> 0
+            }
+
+            if(encounteredX && encounteredO)
+                return size * size
+        }
+
+        return score
     }
 
     private fun initiateOrOnGoing(): Boolean{
@@ -151,7 +290,7 @@ class GameViewModel(private val size: Int): ViewModel() {
     }
 
     private fun isMainDiagonalWin(x: Int, y: Int): Boolean{
-        if(x != y)
+        if(!inMainDiagonal(x, y))
             return false
 
         for(ij in 0 until size) {
@@ -168,7 +307,7 @@ class GameViewModel(private val size: Int): ViewModel() {
     }
 
     private fun isAntiDiagonalWin(x: Int, y: Int): Boolean{
-        if(x + y != size - 1)
+        if(!inAntiDiagonal(x, y))
             return false
 
         for(ij in 0 until size) {
--- app/src/main/java/dev/startsoftware/tictactoe/viewmodels/PlayersViewModel.kt
+++
@@ -1,4 +0,0 @@
-package dev.startsoftware.tictactoe.viewmodels
-
-class PlayersViewModel {
-}
